c2d2c671e9242381e0b8257b9ebd2100106eff04
Mike Boyle <mikesboyle@gmail.com>
2025-08-24 20:12:51 -0400

    changes to trips_unified model that should have been in #9

--- dbt_transformations/models/silver/trips_unified.sql
+++ dbt_transformations/models/silver/trips_unified.sql
@@ -21,25 +21,43 @@ WITH unified_base AS (
     {% if is_incremental() %}
     WHERE _batch_key LIKE '{{ var("month_key") }}%'
     {% endif %}
+),
+
+trips_enriched AS (
+    SELECT
+        *,
+
+        -- Temporal enrichments
+        EXTRACT(HOUR FROM started_at) as start_hour,
+        EXTRACT(DAYOFWEEK FROM started_at) as start_day_of_week,
+        EXTRACT(MONTH FROM started_at) as start_month,
+        CASE
+            WHEN EXTRACT(MONTH FROM started_at) IN (12,1,2) THEN 'Winter'
+            WHEN EXTRACT(MONTH FROM started_at) IN (3,4,5) THEN 'Spring'
+            WHEN EXTRACT(MONTH FROM started_at) IN (6,7,8) THEN 'Summer'
+            ELSE 'Fall'
+        END as start_season,
+
+        -- Boolean flags
+        EXTRACT(DAYOFWEEK FROM started_at) IN (1,7) as is_weekend,
+        start_station_id = end_station_id as is_round_trip
+
+    FROM unified_base
+),
+
+trips_with_geography AS (
+    SELECT
+        t.*,
+        start_station.borough AS start_borough,
+        end_station.borough AS end_borough
+
+    FROM trips_enriched t
+
+    LEFT JOIN {{ ref('silver_stations') }} start_station
+        ON t.start_station_id = start_station.short_name
+
+    LEFT JOIN {{ ref('silver_stations') }} end_station
+        ON t.end_station_id = end_station.short_name
 )
 
--- Final SELECT with enrichments
-SELECT
-    *,
-
-    -- Temporal enrichments
-    EXTRACT(HOUR FROM started_at) as start_hour,
-    EXTRACT(DAYOFWEEK FROM started_at) as start_day_of_week,
-    EXTRACT(MONTH FROM started_at) as start_month,
-    CASE
-        WHEN EXTRACT(MONTH FROM started_at) IN (12,1,2) THEN 'Winter'
-        WHEN EXTRACT(MONTH FROM started_at) IN (3,4,5) THEN 'Spring'
-        WHEN EXTRACT(MONTH FROM started_at) IN (6,7,8) THEN 'Summer'
-        ELSE 'Fall'
-    END as start_season,
-
-    -- Boolean flags
-    EXTRACT(DAYOFWEEK FROM started_at) IN (1,7) as is_weekend,
-    start_station_id = end_station_id as is_round_trip
-
-FROM unified_base
+SELECT * FROM trips_with_geography
