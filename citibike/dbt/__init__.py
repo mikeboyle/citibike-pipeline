295c2be8d6ab5188bc71ecd4dd4972c3bfee35b2
Mike Boyle <mikesboyle@gmail.com>
2025-08-25 11:21:54 -0400

    simple run dbt utility

--- citibike/dbt/__init__.py
+++ citibike/dbt/__init__.py
@@ -1,11 +1,12 @@
 import subprocess
 from pathlib import Path
+from typing import List, LiteralString
 
-def run_dbt_command(command: str) -> None:
+def run_dbt_command(command_args: List[LiteralString] | List[str]) -> None:
     package_dir = Path(__file__).parent.parent
     dbt_dir = package_dir.parent / "dbt_transformations"
     
-    command_args = command.split(" ")
+    command_args = command_args
     result = subprocess.run(
         command_args,
         cwd=dbt_dir,
