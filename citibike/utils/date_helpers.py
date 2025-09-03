634accd8c7c466bdb3fd373701019c524e6fcd62
Mike Boyle <mikesboyle@gmail.com>
2025-09-03 16:40:51 -0400

    update comment

--- citibike/utils/date_helpers.py
+++ citibike/utils/date_helpers.py
@@ -5,7 +5,7 @@ DATETIME_STR_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 def now_nyc_datetime() -> datetime:
     """
-    Return the current datetime in America/New_York time
+    Return the current datetime, converted to America/New_York time
     but with the timezone info removed (so it is a timezone naive datetime
     which BigQuery may need to infer it is a datetime and not a utc timestamp).
     """
